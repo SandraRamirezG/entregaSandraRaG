70e446899441644251ac1ed9143951329eaf6cc2
Sandra Ramirez <136664839+SandraRamirezG@users.noreply.github.com>
2024-05-28 20:00:55 -0400

    crea vite-react

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,20 +1,6 @@
-import React from 'react';
 
-// const NavBar = () => {
-//     return (
-//         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//             <div className="container">
-//                 <a className="navbar-brand" href="#">Brand</a>
-//                 <div className="collapse navbar-collapse" id="navbarNav">
-//                     <ul className="navbar-nav">
-//                         <li className="nav-item">
-//                             <a className="nav-link" href="#">Categoría 1</a>
-//                         </li>
-//                         <li className="nav-item">
-//                             <a className="nav-link" href="#">Categoría 2</a>
-//                         </li>
-//                         import React from 'react';
-//                         import CartWidget from './CartWidget';
+import React from 'react';
+import CartWidget from './CartWidget';
 
 const NavBar = () => {
     return (
@@ -35,22 +21,14 @@ const NavBar = () => {
                         <li className="nav-item">
                             <a className="nav-link" href="#">Auditorias BPM, HACCP, ISO 9001; 2015, Prevención de Riesgos</a>
                         </li>
-
                     </ul>
                 </div>
                 <CartWidget />
             </div>
         </nav>
     );
-}
+};
 
-//                         export default NavBar;
-//                     </ul>
-//                 </div>
-//             </div>
-//         </nav>
-//     );
-// }
-
+export default NavBar;
 // export default NavBar;
 
